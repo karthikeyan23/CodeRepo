f7d5b66c77b1d9d3088a79017f75411b4a0475d5
Karthikeyan Venkatesan <kn.in@live.in>
2017-11-26 06:01:49 +0530

    init angular app

--- ChatBot/DialogFlow/DFTest.js
+++ ChatBot/DialogFlow/DFTest.js
@@ -1,7 +1,7 @@
 var apiai = require('apiai');
- 
+
 var app = apiai("ae02f46f39f94a9e9faa5d05777d7f01");
- 
+
 var request = app.textRequest('HI', {
     sessionId: 'test'
 });
@@ -9,9 +9,9 @@ var request = app.textRequest('HI', {
 request.on('response', function(response) {
     console.log(response);
 });
- 
+
 request.on('error', function(error) {
     console.log(error);
 });
- 
+
 request.end();
